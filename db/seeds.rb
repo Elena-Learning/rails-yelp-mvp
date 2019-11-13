740a1b875ca52f72dc88c40da063f3abe024e9a7
Elena-Learning <elena.urban.professionell@gmail.com>
2019-11-13 15:11:45 +0100

    finished restaurant controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+res1 = Restaurant.create(name: 'McDonalds', address: 'Berlin', category: 'french')
+Review.create(content: 'soso', rating: 1, restaurant_id: res1.id)
+Review.create(content: 'great', rating: 5, restaurant_id: res1.id)
+
+res2 = Restaurant.create(name: 'Hum', address: 'Prenzlauer Berg', category: 'chinese')
+Review.create(content: 'amazing', rating: 3, restaurant_id: res2.id)
+Review.create(content: 'super', rating: 4, restaurant_id: res2.id)
+
+res3 = Restaurant.create(name: 'Pasta e Vino', address: 'Prenzlauer Berg', category: 'italian')
+Review.create(content: 'healthy and delicious', rating: 3, restaurant_id: res3.id)
+Review.create(content: 'awesome', rating: 4, restaurant_id: res3.id)
